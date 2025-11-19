90f0ca75b5d9a8a4d7dd9be65ed42135e12b4248
AshishLekhyani <alekhyanisbi@gmail.com>
2025-11-19 17:16:17 +0530

    make delivery options interactive

--- data/cart.js
+++ data/cart.js
@@ -23,7 +23,7 @@ export function addToCart(productId) {
     cart.push({
       productId,
       quantity,
-      deliveryOptionId: '1' 
+      deliveryOptionId: '1'
     });
   }
 
@@ -68,10 +68,23 @@ export function removeFromCart(productId) {
 export function updateCart(productId, newQuantity) {
 
   cart.forEach((cartItem) => {
-    if(cartItem.productId === productId) {
+    if (cartItem.productId === productId) {
       cartItem.quantity = newQuantity;
     }
   });
 
+  saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
   saveToStorage();
 }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart, calculateCartQuantity, updateCart } from '../data/cart.js';
+import { cart, removeFromCart, calculateCartQuantity, updateCart, updateDeliveryOption } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
@@ -149,7 +149,7 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
 
         html +=
             `
-            <div class="delivery-option">
+            <div class="delivery-option js-delivery-option" data-product-id = "${matchingProduct.id}" data-delivery-option-id = "${deliveryOption.id}">
                 <input type="radio"
                     ${isChecked ? 'checked' : ''}
                     class="delivery-option-input"
@@ -168,3 +168,11 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
 
     return html;
 }
+
+document.querySelectorAll('.js-delivery-option').forEach((option) => {
+    option.addEventListener('click', () => {
+        const { productId, deliveryOptionId } = option.dataset;
+
+        updateDeliveryOption(productId, deliveryOptionId);
+    });
+});
